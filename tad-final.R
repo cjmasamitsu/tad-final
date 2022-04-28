9af71d54232da60a3bd18a44226132bbf7bb43b3
cjmasamitsu <cjmasamitsu@gmail.com>
2022-04-27 20:07:18 -0400

    Initial commit

--- tad-final.R
+++ tad-final.R
@@ -1,5 +1,35 @@
-1+1
-# Test repo
+library(tidyverse)
+library(shiny)
+# install.packages("quanteda")
+library(quanteda)
 
-# Test test test  
-5+5+5
+# Data Preparation --------------------------------------------------------
+
+
+
+# Shiny UI ----------------------------------------------------------------
+
+ui <- fluidPage(
+  # Main Title
+  titlePanel("How are United States Senators using Twitter to engage their constituents?"),
+  navlistPanel(
+    
+    # Category 1
+    "X y z",
+    tabPanel("By State",
+             mainPanel(
+               h2("test 1"),
+               h4("test 2")
+             ))
+  )
+)
+
+
+# Shiny Server ------------------------------------------------------------
+
+server <- function(input, output){
+  output$plot1 <- renderPlotly({
+    ggplotly(viz_one)
+    
+  })
+}
